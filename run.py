08875f1e1b1fff242b3134d37474d0512f8237c0
joshhowolahbi <josh.howolahbi@gmail.com>
2021-07-27 14:31:06 +0100

    doc strings

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
+""" The run file to initialize the game """
+
 from game import gameplay
 
 if __name__ == "__main__":
     print("""Welcome to DEAD AND WOUNDED!!!
-    Try to guess the four digit numbers with no duplicate values\n""")
+    Try to guess the four digits number with no duplicate values\n""")
 
     response = 'Y'
-    # checking if to continue playing game or exit
+    # check to continue or exit game
     while response == "Y" or response == "YES":
         gameplay()
         print("")
         response = input("Would you like to play again? (Y/N): ").upper()
         print("")
-    else:
-        print("Thank You for Playing!")
+
+    # end of game
+    print("Thank You for Playing!")
